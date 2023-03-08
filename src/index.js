7ebb030779c42984e3e025bf860e76066a00e4eb
hokkyar <hokkyaryasta@gmail.com>
2023-03-08 13:05:01 +0700

    edit not found route

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ app.use(session({
 app.use('/api', apiRoutes)
 app.use('/admin', adminRoutes)
 
-app.use((req, res, next) => res.status(404).render('not-found'))
-app.use((err, req, res, next) => res.status(500).send({ message: 'Internal Server Error', error: err }))
+app.use((req, res, next) => res.status(404).render('pages/not-found'))
+// app.use((err, req, res, next) => res.status(500).send({ message: 'Internal Server Error', error: err }))
 
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server Running at PORT ${PORT}`))
